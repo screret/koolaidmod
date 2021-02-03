e293e0937128f957df30792720253b25ed2afe5f
screret <68943070+screret@users.noreply.github.com>
2021-02-03 16:21:32 +0200

    removed objectholder

--- src/main/java/koolaidmod/handlers/RegistryHandler.java
+++ src/main/java/koolaidmod/handlers/RegistryHandler.java
@@ -1,33 +1,28 @@
-package koolaid.mod.handlers;
+package koolaidmod.handlers;
 
-import koolaid.mod.Base;
-import koolaid.mod.blocks.test_block;
-import koolaid.mod.handlers.client.ModelRegistrationHandler;
-import koolaid.mod.init.ModBlocks;
-import koolaid.mod.init.ModItems;
-import koolaid.mod.util.RegistryUtil;
+import koolaidmod.Base;
+import koolaidmod.blocks.test_block;
+import koolaidmod.init.ModBlocks;
+import koolaidmod.init.ModItems;
+import koolaidmod.util.RegistryUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.logging.Logger;
+
 @Mod.EventBusSubscriber
-@GameRegistry.ObjectHolder(Base.MODID)
 public class RegistryHandler {
-
-
-    public static void newRegistry(final RegistryEvent.NewRegistry event){
-
-    }
 
     @SubscribeEvent
     public static void registerItems(@NotNull final RegistryEvent.Register<Item> event){
+        Logger logger = Logger.getLogger(Base.MODID);
         final Item[] items = {
                 RegistryUtil.setItemName(ModItems.KOOL_AID, "kool_aid").setCreativeTab(Base.MOD_TAB),
                 RegistryUtil.setItemName(ModItems.JUICE_HELMET, "juice_helmet").setCreativeTab(Base.MOD_TAB),
@@ -40,14 +35,21 @@ public class RegistryHandler {
         };
         event.getRegistry().registerAll(items);
         event.getRegistry().registerAll(itemBlocks);
+        for(Item item : items)
+            logger.warning("Registered item: " + item.getRegistryName());
+        for(Item item : itemBlocks)
+            logger.warning("Registered item: " + item.getRegistryName());
     }
 
     @SubscribeEvent
     public static void registerBlocks(@NotNull final RegistryEvent.Register<Block> event){
+        Logger logger = (Logger) Logger.getLogger(Base.MODID);
         final Block[] blocks = {
                 RegistryUtil.setBlockName(new test_block(Material.ROCK), "test_block").setCreativeTab(Base.MOD_TAB),
         };
         event.getRegistry().registerAll(blocks);
+        for(Block block : blocks)
+            logger.warning("Registered item: " + block.getRegistryName());
     }
 
     /*@SubscribeEvent(priority = EventPriority.HIGHEST)
