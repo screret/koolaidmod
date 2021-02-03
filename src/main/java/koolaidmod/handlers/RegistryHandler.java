0e305675fa91d027155848c0bc38969e4513e7c2
screret <68943070+screret@users.noreply.github.com>
2021-02-03 17:46:45 +0200

    changed registeration

--- src/main/java/koolaidmod/handlers/RegistryHandler.java
+++ src/main/java/koolaidmod/handlers/RegistryHandler.java
@@ -5,9 +5,12 @@ import koolaidmod.blocks.test_block;
 import koolaidmod.init.ModBlocks;
 import koolaidmod.init.ModItems;
 import koolaidmod.init.PotionInit;
+import koolaidmod.items.armor.ModArmor;
+import koolaidmod.materials.ModMaterials;
 import koolaidmod.util.RegistryUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.potion.PotionType;
@@ -19,30 +22,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.logging.Logger;
 
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(modid = Base.MODID)
 public class RegistryHandler {
 
     Logger logger = Logger.getLogger(Base.MODID);
-
-    @SubscribeEvent
-    public void registerItems(@NotNull final RegistryEvent.Register<Item> event){
-        final Item[] items = {
-                RegistryUtil.setItemName(ModItems.KOOL_AID, "kool_aid").setCreativeTab(Base.MOD_TAB),
-                RegistryUtil.setItemName(ModItems.JUICE_HELMET, "juice_helmet").setCreativeTab(Base.MOD_TAB),
-                RegistryUtil.setItemName(ModItems.JUICE_CHESTPLATE, "juice_chestplate").setCreativeTab(Base.MOD_TAB),
-                RegistryUtil.setItemName(ModItems.JUICE_LEGGINGS, "juice_leggings").setCreativeTab(Base.MOD_TAB),
-                RegistryUtil.setItemName(ModItems.JUICE_BOOTS, "juice_boots").setCreativeTab(Base.MOD_TAB),
-        };
-        final Item[] itemBlocks = {
-                RegistryUtil.setItemName(new ItemBlock(ModBlocks.TEST_BLOCK), "test_block").setCreativeTab(Base.MOD_TAB)
-        };
-        event.getRegistry().registerAll(items);
-        event.getRegistry().registerAll(itemBlocks);
-        for(Item item : items)
-            logger.warning("Registered item: " + item.getRegistryName());
-        for(Item item : itemBlocks)
-            logger.warning("Registered item: " + item.getRegistryName());
-    }
 
     @SubscribeEvent
     public void registerBlocks(@NotNull final RegistryEvent.Register<Block> event){
@@ -51,7 +34,27 @@ public class RegistryHandler {
         };
         event.getRegistry().registerAll(blocks);
         for(Block block : blocks)
-            logger.warning("Registered item: " + block.getRegistryName());
+            logger.warning("Registered item: " + block.getRegistryName().toString());
     }
 
+    @SubscribeEvent
+    public void registerItems(@NotNull final RegistryEvent.Register<Item> event){
+        final Item[] items = {
+                RegistryUtil.setItemName(new Item(), "kool_aid").setCreativeTab(Base.MOD_TAB),
+
+                RegistryUtil.setItemName(new ModArmor(ModMaterials.JUICE_ARMOR, EntityEquipmentSlot.HEAD), "juice_helmet").setCreativeTab(Base.MOD_TAB),
+                RegistryUtil.setItemName(new ModArmor(ModMaterials.JUICE_ARMOR, EntityEquipmentSlot.CHEST), "juice_chestplate").setCreativeTab(Base.MOD_TAB),
+                RegistryUtil.setItemName(new ModArmor(ModMaterials.JUICE_ARMOR, EntityEquipmentSlot.LEGS), "juice_leggings").setCreativeTab(Base.MOD_TAB),
+                RegistryUtil.setItemName(new ModArmor(ModMaterials.JUICE_ARMOR, EntityEquipmentSlot.FEET), "juice_boots").setCreativeTab(Base.MOD_TAB),
+        };
+        final Item[] itemBlocks = {
+                new ItemBlock(ModBlocks.TEST_BLOCK).setRegistryName(ModBlocks.TEST_BLOCK.getRegistryName())
+        };
+        event.getRegistry().registerAll(items);
+        event.getRegistry().registerAll(itemBlocks);
+        for(Item item : items)
+            logger.warning("Registered item: " + item.getRegistryName().toString());
+        for(Item item : itemBlocks)
+            logger.warning("Registered item: " + item.getRegistryName().toString());
+    }
 }
